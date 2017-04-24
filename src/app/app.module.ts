13c9583e4ea34a9e386d09776f3856820c9695e7
dasensio13 <dasensio13@gmail.com>
2017-04-24 11:03:47 +0200

    7. HTTP 1/2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
@@ -9,8 +10,12 @@ import { DashboardComponent } from './dashboard.component';
 import { HeroService } from './hero.service';
 import { AppRoutingModule } from './app-routing.module';
 
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule, HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),],
   declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent ],
   bootstrap:    [ AppComponent ],
   providers:    [ HeroService ]
--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -13,18 +13,35 @@ export class HeroesComponent implements OnInit {
   subtitle = 'My Heroes';
   heroes: Hero[];
   selectedHero: Hero;
-  constructor(private heroService: HeroService, private router: Router) { };
+  constructor(private heroService: HeroService, private router: Router) { }
   ngOnInit(): void {
     this.getHeroes();
-  };
+  }
   getHeroes(): void {
-    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
     //timeout this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
-  };
+  }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-  };
+  }
   gotoDetail(): void {
     this.router.navigate(['/detail', this.selectedHero.id]);
-  };
+  }
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
+  delete(hero: Hero): void {
+    this.heroService
+        .delete(hero.id)
+        .then(() => {
+          this.heroes = this.heroes.filter(h => h !== hero);
+          if (this.selectedHero === hero) { this.selectedHero = null; }
+        });
+  }
 }
